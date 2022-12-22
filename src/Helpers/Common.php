e336c3eaa90967528d2f495ccd3986556f745a9b
Mulligan_1499 <55077411+gabin9999@users.noreply.github.com>
2022-12-22 16:37:49 +0700

    Update Common.php

--- src/Helpers/Common.php
+++ src/Helpers/Common.php
@@ -6,15 +6,6 @@ use GuzzleHttp\Client;
 
 class Common {
 
-    const ERR_NONE = 0;
-    protected $merchantId = 400734;
-    protected $apiKey = 'iIcJMU32wzuemkQUZ495CXvqnhnxKdR0';
-    protected $apiSecret = 'D8BB7icFvOTgQ9xgS2li0OufgWnDQLMm';
-    protected $apiUrl = 'https://dev-api.baokim.vn';
-    protected $_jwt;
-    const TOKEN_EXPIRE = 600;
-
-
     function requestAPI($method, $url, $headers, $query = [], $params = [])
     {
         $res = new \stdClass();
